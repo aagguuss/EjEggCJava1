b11fa6c596bf8a7c7535946d601f9e911ac1496b
aagguuss <126991146+aagguuss@users.noreply.github.com>
2023-03-08 18:58:12 -0300

    Add files via upload

---
+++ EjercicosJava1EggC/ejercicios_Java1.4/src/ejercicios_java1/pkg4/Ejercicios_Java14.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios_java1.pkg4;
+
+/**
+ *
+ * @author agust
+ */
+public class Ejercicios_Java14 {
+
+    /**
+     * @param args the command line arguments
+     * 
+     */
+    public static void main(String[] args) {
+        int Celcius = 29;
+        int F =32 + (9 * Celcius / 5);
+        System.out.println("la temperatura en de "+ F +" Faranheit");
+    }
+    
+}
